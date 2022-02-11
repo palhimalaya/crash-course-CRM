f3a4b69c8e354233d2ce34cc58206946859a1586
palhimalaya <himalayapal123@gmail.com>
2022-02-11 13:44:29 +0545

    Django database and rendering data to templates

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,3 +1,54 @@
+import email
+from re import T
+from unicodedata import category, name
 from django.db import models
+from django.forms import FloatField
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    STATUS = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    
+    
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=STATUS)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tages = models.ManyToManyField(Tag)
+    
+    
+    def __str__(self):
+        return self.name
+class Order(models.Model):
+    STATUS = (
+             ('Pending', 'Pending'),
+             ('Out for delivery', 'Out for delivery'),
+             ('Delivered', 'Delivered'),
+             )
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS )
+    
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,12 +1,27 @@
+from multiprocessing import context
+from telnetlib import STATUS
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    
+    total_customers = customers.count() 
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    
+    
+    context = {'orders':orders,'total_customers':total_customers, 'total_orders':total_orders, 'customers':customers, 'delivered':delivered, 'pending':pending}
+    
+    return render(request,'accounts/dashboard.html',context)
 
 def customer(request):
     return render(request,'accounts/customer.html')
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products = Product.objects.all()
+    return render(request,'accounts/products.html',{'products':products})
